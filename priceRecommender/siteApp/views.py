da0fea1d08827083d4cd96e21110b4b4c20f00f7
hiroCat <miki2189@gmail.com>
2016-07-12 19:36:30 +0200

    last version

--- priceRecommender/siteApp/views.py
+++ priceRecommender/siteApp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.core import validators
-from django.forms.util import ErrorList
+import django.forms.utils
 
 from .forms import AirbnbRequestForm
 
@@ -33,6 +33,7 @@ def index(request):
                 form.hostAbout = request.POST['valuehostAbout']
                 form.hostIdentityVerified = request.POST['valuehostIdentityVerified']
                 form.extraPeople = request.POST['valueExtraPeople']
+                form.postalCode = helperTranformationPostalCode(form.data['street'],form.data['number'])
                 #form.save()
                 price = processData(form)
                 outputPrice ="{0:.2f}".format(price[0])
@@ -63,20 +64,27 @@ def helperTransformation(inputString):
     return output
 
 def secondValidation(form):
-    # Remove when we fix the latitude thing
-    # if form.latitude == 0 or form.longitude == 0 or not checkLatitudeLong(form.latitude,form.longitude):
-    #     errors = form._errors.setdefault("street", ErrorList())
+    #Remove when we fix the latitude thing
+    if form.latitude == 0 or form.longitude == 0 or not checkLatitudeLong(form.latitude,form.longitude):
+        errors = form._errors.setdefault("street")#, ErrorList())
+        #errors.append(u"Wrong Combination or not from BCN")
+        return False
+    # if form.latitude == 0 or form.longitude == 0:
+    #     errors = form._errors.setdefault("street")#, ErrorList())
     #     errors.append(u"Wrong Combination or not from BCN")
     #     return False
-    if form.latitude == 0 or form.longitude == 0:
-        errors = form._errors.setdefault("street", ErrorList())
-        errors.append(u"Wrong Combination or not from BCN")
-        return False
     return True
 
 
 def helperTransformationGPS(street, number):
     return get_coordinates(street+" "+number+",Barcelona")
+
+def helperTranformationPostalCode(street, number):
+    result = get_geocode_location(street+" "+number+",Barcelona")
+    if result is None:
+        return ""
+    arrayResult = result.address.split(",")
+    return arrayResult[6]
 
 #TODO helpers to move
 
@@ -87,19 +95,20 @@ def createVectorCaracteristicsNewUser(form):
     return final
 
 def createJSONString(form):
-    x = "\"street\":{0},\"zipcode\":{1},\"latitude\":{2},\"longitude\":{3},\"host_about\":{4}," \
-        "\"host_listings_count\":{5},\"host_identity_verified\":{6},\"availability_30\":{7},\"availability_60\":{8},\"availability_90\":{9}," \
-        "\"availability_365\":{10},\"cancellation_policy\":{11},\"bathrooms\":{12},\"bedrooms\":{13},\"beds\":{14}," \
-        "\"accommodates\":{15},\"guests_included\":{16},\"extra_people\":{17},\"security_deposit\":{18}"
+
+    x = "\"zipcode\":{0},\"latitude\":{1},\"longitude\":{2},\"host_about\":{3}," \
+        "\"host_listings_count\":{4},\"host_identity_verified\":{5},\"availability_30\":{6},\"availability_60\":{7},\"availability_90\":{8}," \
+        "\"availability_365\":{9},\"cancellation_policy\":{10},\"bathrooms\":{11},\"bedrooms\":{12},\"beds\":{13}," \
+        "\"accommodates\":{14},\"guests_included\":{15},\"extra_people\":{16},\"security_deposit\":{17}"
 
     stringAmenities = "tv,internet,kitchen,ac,smoking,hottub,heating,family,events,dryer,smoke,shampoo,elevator,washer,intercom,essentials,lock,24hourcheckin,hangers,laptopf,hairdryer,iron,firstaidkit,cabletv,fireplace,extinguisher,breakfast,pets,petsallowed,parking,safetycard,doorman,wheelchair,carbondetector,gym,washerdryer,pool"
     stringVerifications ="verified_email,verified_phone,verified_facebook,verified_linkedin,verified_google,verified_jumio,verified_reviews,verified_manual"
-    stringNeighborhoods = "baro_de_viver,can_baro,can_peguera,canyelles,ciutat_meridiana,diagonal_mar_i_el_front_maritim_del_poblenou,horta,hostafrancs,montbau,navas,pedralbes,porta,provencals_del_poblenou,sant_andreu,sant_antoni,sant_genis_dels_agudells,sant_gervasi_-_galvany,sant_gervasi_-_la_bonanova,sant_marti_de_provencals,_santa_caterina_i_la_ribera,sants,sants_-_badal,sarria,torre_baro,vallcarca_i_els_penitents,vallvidrera,verdun,vilapicina_i_la_torre_llobeta,el_baix_guinardo,el_barri_gotic,el_besos_i_el_maresme,el_bon_pastor,el_camp_d_en_grassot_i_gracia_nova,el_camp_de_l_arpa_del_clot,el_carmel,el_clot,el_coll,el_congres_i_els_indians,el_fort_pienc,el_guinardo,el_parc_i_la_llacuna_del_poblenou,el_poble_sec,el_poblenou,el_putxet_i_el_farro,el_raval,el_turo_de_la_peira,l_antiga_esquerra_de_l_eixample,la_barceloneta,la_bordeta,la_clota,la_dreta_de_l_eixample,la_font_d_en_fargues,la_font_de_la_guatlla,la_guineueta,la_marina_de_port,la_marina_del_prat_vermell,la_maternitat_i_sant_ramon,la_nova_esquerra_de_l_eixample,la_prosperitat,la_sagrada_familia,la_sagrera,la_salut,la_teixonera,la_trinitat_nova,la_trinitat_vella,la_vall_d_hebron,la_verneda_i_la_pau,la_vila_olimpica_del_poblenou,la_vila_de_gracia,les_corts,les_roquetes,les_tres_torres"
+    stringNeighborhoods = "baro_de_viver,can_baro,can_peguera,canyelles,ciutat_meridiana,diagonal_mar_i_el_front_maritim_del_poblenou,horta,hostafrancs,montbau,navas,pedralbes,porta,provencals_del_poblenou,sant_andreu,sant_antoni,sant_genis_dels_agudells,sant_gervasi_-_galvany,sant_gervasi_-_la_bonanova,sant_marti_de_provencals,sant_pere,sants,sants_-_badal,sarria,torre_baro,vallcarca_i_els_penitents,vallvidrera,verdun,vilapicina_i_la_torre_llobeta,el_baix_guinardo,el_barri_gotic,el_besos_i_el_maresme,el_bon_pastor,el_camp_d_en_grassot_i_gracia_nova,el_camp_de_l_arpa_del_clot,el_carmel,el_clot,el_coll,el_congres_i_els_indians,el_fort_pienc,el_guinardo,el_parc_i_la_llacuna_del_poblenou,el_poble_sec,el_poblenou,el_putxet_i_el_farro,el_raval,el_turo_de_la_peira,l_antiga_esquerra_de_l_eixample,la_barceloneta,la_bordeta,la_clota,la_dreta_de_l_eixample,la_font_d_en_fargues,la_font_de_la_guatlla,la_guineueta,la_marina_de_port,la_marina_del_prat_vermell,la_maternitat_i_sant_ramon,la_nova_esquerra_de_l_eixample,la_prosperitat,la_sagrada_familia,la_sagrera,la_salut,la_teixonera,la_trinitat_nova,la_trinitat_vella,la_vall_d_hebron,la_verneda_i_la_pau,la_vila_olimpica_del_poblenou,la_vila_de_gracia,les_corts,les_roquetes,les_tres_torres"
     stringtypeApp = "apartment,bed_&_breakfast,boat,bungalow,cabin,camper_rv,chalet,condominium,dorm,house,loft,other,tent,townhouse,villa,yurt"
     stringEntireAp = "entire_home_apt,private_room,shared_room"
     stringTypeBed = "airbed,couch,futon,pull-out_sofa,real_bed"
 
-    x = x.format(0, 0, form.latitude, form.longitude, form.hostAbout,
+    x = x.format(form.postalCode, form.latitude, form.longitude, form.hostAbout,
                  form.data['hostListingsCount'], form.hostIdentityVerified, form.data['availability30'], form.data['availability60'], form.data['availability90'],
                  form.data['availability365'], form.data['cancellationPolicy'], form.data['bathrooms'], form.data['bedrooms'], form.data['beds'],
                  form.data['accomodates'], form.data['guestsIncluded'], form.extraPeople, form.apartmentDeposit)
@@ -206,21 +215,23 @@ def get_coordinates(address_string):
     '''This function finds the latitude and longitude given an address string.
     If no address is found returs 0,0 (location in the ocean)
     It does not privide more than one result'''
+    location = get_geocode_location(address_string)
+    if location is None:
+        return 0,0
+    else:
+        return location.latitude,location.longitude
+
+def get_geocode_location(address_string):
     g = geocoders.Nominatim()
     try:
-        location = g.geocode(address_string, timeout=10,exactly_one=True)
-        if location is None:
-            return 0,0
-        else:
-            return location.latitude, location.longitude
+        return g.geocode(address_string, timeout=10,exactly_one=True)
     except:
-        return 0,0
+        return None
 
-def find_mydistance(lat,lng,filename,neighbors_number=100):
+def find_mydistance(lat,lng,data,neighbors_number=25):
     '''This function finds the first neighbors to a point given a set of coordinates from a file.
         It calclutates the distance from the chosen point to all the neighbors,
         and returns the median distance to the neighbors'''
-    data=pd.read_csv(filename, sep=";")
     neigh = NearestNeighbors(n_neighbors=neighbors_number, metric='euclidean', n_jobs=-1)
     neigh.fit(data.loc[:,('latitude','longitude')])
     my_coordinates=np.array([lat, lng])
@@ -234,17 +245,14 @@ def find_mydistance(lat,lng,filename,neighbors_number=100):
     return np.median(d_meters)
 
 def check_mypos(mydistance, control_parameter):
-    '''This function checks if the ditance of the point is within the chose accaptance range,
+    '''This function checks if the distance of the point is within the chose accaptance range,
     and returns a boolean value'''
     return mydistance<control_parameter
 
 def checkLatitudeLong(latitude, longitude):
-    filename="siteApp/pklObjects/step2_output_neigh100threshold8.csv"
-    #the following bit could be useful if we try more than one city, otherwise just usening the file
-    #save_control_par 41 2.p in the pickle call will be enough, and much faster as you dont reload the data
-    data=pd.read_csv(filename, sep=";")
+    data=pd.read_csv("siteApp/pklObjects/step2_output.csv", sep=";")
     central_lat=np.mean(data.loc[:,('latitude')].values)
     central_long=np.mean(data.loc[:,('longitude')].values)
-    control_parameter=pickle.load(open("siteApp\pklObjects\geo_outliers\save_control_par"+" %.0f %.0f.p"% (central_lat,central_long), "rb" ) )
-    return  check_mypos(find_mydistance(latitude,longitude,filename),control_parameter)
+    control_parameter=pickle.load(open("siteApp\pklObjects\save_control.p", "rb"))
+    return  check_mypos(find_mydistance(latitude,longitude,data),control_parameter)
 #TODO
